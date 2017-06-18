50fc4d511adabe59b73e8098549afb2dc7207997
Hiroyuki Komatsuzawa <kohiandrew@gmail.com>
2017-06-18 22:59:28 +0900

    Add Scroll

--- js-practice.js
+++ js-practice.js
@@ -1,10 +1,22 @@
 $(function () {
-  var navMenu = $('.nav-menu');
-  var globalNav = $('.global-nav');
+  var navMenu = $('.nav-menu')
+   , globalNav = $('.global-nav')
+   , topLink = $('#topLink')
+   , bottomLink = $('#bottomLink')
+   , bottomOffset = $(document).height();
+   ;
 
   navMenu.click(function () {
     navMenu.toggleClass('active');
     globalNav.toggleClass('active');
+  });
+
+  topLink.click(function (e) {
+    $("html,body").animate({scrollTop: 0});
+  });
+
+  bottomLink.click(function (e) {
+    $("html,body").animate({scrollTop: bottomOffset});
   });
 
 });
